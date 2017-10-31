9bf98948713dfc00ce9cc1e3c9c84067e660cc2a
Jose Ramirez <jdevla@protonmail.com>
2017-10-31 15:26:06 -0700

    Update final.py

--- final.py
+++ final.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, jsonify, \
     url_for, flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
-from database_setup import NC, Base, GameList, User
+from database_setup import NC, Base, GameList, Userdddddd
 
 from flask import session as login_session
 import random
