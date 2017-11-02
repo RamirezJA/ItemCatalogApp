675e6f2d3d439fca1ef060b344cbed1c06fac39d
Jose Ramirez <jdevla@protonmail.com>
2017-11-01 22:00:47 -0700

    added cascade method

--- database_setup.py
+++ database_setup.py
@@ -24,6 +24,7 @@ class NC(Base):
     name = Column(String(250), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
+    list = relationship('GameList', cascade='all, delete-orphan')
 
     @property
     def serialize(self):
